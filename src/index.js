1ebea3c27869e972181f18ac10cdb9a7f1939c9b
Evelyn V Pírez <113993873+eve-vp@users.noreply.github.com>
2023-07-24 12:29:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+const inputTextElement = document.getElementById('inputText');
+const shiftAmountElement = document.getElementById('shiftAmount');
+const encodedTextElement = document.getElementById('encodedText');
+const decodedTextElement = document.getElementById('decodedText');
+
+function encode() {
+  const inputText = inputTextElement.value;
+  const shiftAmount = parseInt(shiftAmountElement.value);
+  const encodedText = caesarCipher(inputText, shiftAmount);
+  encodedTextElement.value = encodedText;
+  decodedTextElement.value = ''; // Clear the decoded text area
+}
+
+function decode() {
+  const inputText = inputTextElement.value;
+  const shiftAmount = parseInt(shiftAmountElement.value);
+  // Decoding is just encoding with a negative shift
+  const decodedText = caesarCipher(inputText, -shiftAmount);
+  decodedTextElement.value = decodedText;
+}
+
+function clearText() {
+  inputTextElement.value = '';
+  encodedTextElement.value = '';
+  decodedTextElement.value = '';
+}
