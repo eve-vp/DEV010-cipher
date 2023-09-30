93ca25142146ea71211ad470975abc1e4c5463bd
Evelyn V Pírez <113993873+eve-vp@users.noreply.github.com>
2023-09-30 13:53:25 -0400

    Update index.js
    Comentarios código index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import caesarCipher from './cipher.js';
 // index.js
-
+// Eventos del Don de atributo ID
 const inputTextElement = document.getElementById('inputText');
+// campo donde se ingresa un valor numérico, como la cantidad de desplazamiento en un cifrado César.//
 const shiftAmountElement = document.getElementById('shiftAmount');
+//se usa para acceder a un área o elemento donde se muestra o ingresa texto que se desea cifrar.//
 const encodeTextElement = document.getElementById('encodeText');
+//se usa para acceder a un área o elemento donde se muestra o ingresa texto que se desea descifrar.//
 const decodeTextElement = document.getElementById('decodeText');
 
 function encode() {
